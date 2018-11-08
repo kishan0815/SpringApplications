3cd88a3cc9626701115f30da1cfbd863c67f02f8
Kishan Kumar <kishan.k@tadigital.com>
2018-11-09 00:42:58 +0530

    Added source code for Spring Multiple View Resolvers

---
+++ SpringMultipleViewResolvers/src/com/tadigital/controllers/RegisterController.java
@@ -0,0 +1,40 @@
+package com.tadigital.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tadigital.entity.Vendor;
+import com.tadigital.service.VendorService;
+
+@Controller
+@RequestMapping(value = "/register")
+public class RegisterController {
+	
+	private VendorService vs;
+	
+	@Autowired
+	public RegisterController(VendorService vs) {
+		this.vs = vs;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String loadRegisterForm() {
+		return "registerForm";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String registerTask(@ModelAttribute Vendor v,HttpServletRequest req) {
+	
+		boolean b = vs.registerVendor(v);
+		
+		if(b) {
+			return "registrationSuccess";
+		}
+		return "registrationFailure";
+	}
+}
