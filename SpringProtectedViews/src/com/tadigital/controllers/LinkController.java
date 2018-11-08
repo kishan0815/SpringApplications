e1f65f19fcad7706454607915534129478f597b6
Kishan Kumar <kishan.k@tadigital.com>
2018-11-08 21:05:56 +0530

    Added source code for Spring Protected Views

---
+++ SpringProtectedViews/src/com/tadigital/controllers/LinkController.java
@@ -0,0 +1,15 @@
+package com.tadigital.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LinkController {
+	
+	@RequestMapping(value= {"/","/home"})
+	public String reloadHome() {
+		return "/WEB-INF/views/index.jsp";
+	}
+
+}
---
+++ SpringProtectedViews/src/com/tadigital/controllers/LoginController.java
@@ -0,0 +1,40 @@
+package com.tadigital.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tadigital.entity.Vendor;
+import com.tadigital.service.VendorService;
+
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+	
+	private VendorService vs;
+	
+	@Autowired
+	public LoginController(VendorService vs) {
+		this.vs = vs;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String loadLoginForm() {
+		return "html/loginForm.html";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String loginTask(@ModelAttribute Vendor v, HttpServletRequest req) {
+		
+		boolean b = vs.loginVendor(v);
+		
+		if(b) {
+			return "/WEB-INF/views/loginSuccess.jsp";
+		}
+		return "/WEB-INF/views/loginFailure.jsp";
+	}
+}
